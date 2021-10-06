281fda7c288806a7ab8c739f7fcda03be14da440
akiroz <git@akiroz.life>
2021-10-06 10:59:24 +0800

    Iambic: Refactor DOM out of keyer

--- iambic/index.js
+++ iambic/index.js
@@ -1,21 +1,121 @@
 
-function wpm2unit(wpm) {
-    return (60 * 1000) / (50 * wpm);
+class Modulator {
+    enable = true;
+    actx = new AudioContext();
+    osc = this.actx.createOscillator();
+    gain = this.actx.createGain();
+    band = this.actx.createBiquadFilter();
+    
+    constructor() {
+        this.osc.frequency.value = 550;
+        this.osc.start();
+        this.osc.connect(this.gain);
+        this.gain.gain.value = 0;
+        this.gain.connect(this.band);
+        this.band.type = "bandpass";
+        this.band.frequency.value = 550;
+        this.band.Q.value = 3;
+        this.band.connect(this.actx.destination);
+    }
+
+    setFreq(hz) {
+        this.osc.frequency.value = hz;
+        this.band.frequency.value = hz;
+    }
+
+    output(high) {
+        this.gain.gain.value = (this.enable && high) ? 1 : 0;
+    }
+
 }
 
-let swapKeys = false;
-function mapKey(code) {
-    switch(code) {
-        case "KeyZ":
-        case "Comma":
-            return swapKeys ? "-" : ".";
-        case "KeyX":
-        case "Period":
-            return swapKeys ? "." : "-";
+class Keyer {
+    // Controls
+    wpm = 15;
+    modeB = false;
+    dit = false;
+    dah = false;
+    // Internal State
+    prevKey = null;
+    nextKey = null;
+    idleUnits = 7;
+    code = "";
+    // Outputs
+    onKey = () => {};
+    onWord = () => {};
+    onTxStart = () => {};
+    onTxStop = () => {};
+
+    constructor() {
+        setTimeout(() => this.run(), 0);
+    }
+
+    queue(key) {
+        if(key && !this.nextKey) this.nextKey = key;
+    }
+
+    timeout(unit) {
+        const unitMs = (60 * 1000) / (50 * this.wpm);
+        return new Promise(r => setTimeout(r, unit * unitMs));
+    }
+
+    async append(key) {
+        this.onKey(key);
+        if(key === "\n") {
+            this.onWord("\n");
+        }
+        if(key === " ") {
+            this.onWord(this.code);
+            this.code = "";
+        }
+        if(key === ".") {
+            this.code += ".";
+            this.onTxStart();
+            await this.timeout(1);
+            this.onTxStop();
+            await this.timeout(1);
+        }
+        if(key === "-") {
+            this.code += "-";
+            this.onTxStart();
+            await this.timeout(3);
+            this.onTxStop();
+            await this.timeout(1);
+        }
+    }
+
+    async run() {
+        while(true) {
+            let currKey = null;
+            if(this.nextKey) {
+                [this.nextKey, currKey] = [null, this.nextKey];
+            } else if(this.dit && this.dah) {
+                if(this.prevKey === ".") currKey = "-";
+                if(this.prevKey === "-") currKey = ".";
+            } else if(this.dit) {
+                currKey = ".";
+            } else if(this.dah) {
+                currKey = "-";
+            } else {
+                const iu = this.idleUnits++;
+                if(iu < 1 && this.idleUnits >= 1 && this.modeB) {
+                    if(this.prevKey === ".") await this.append("-");
+                    if(this.prevKey === "-") await this.append(".");
+                    continue;
+                }
+                if(iu < 3 && this.idleUnits >= 3) this.append(" ");
+                if(iu < 7 && this.idleUnits >= 7) this.append("\n");
+                await this.timeout(1);
+                continue;
+            }
+            await this.append(currKey);
+            this.prevKey = currKey;
+            this.idleUnits = 0;
+        }
     }
 }
 
-const morseDecode = {  
+const morseCode = {  
     "-----":"0",
     ".----":"1",
     "..---":"2",
@@ -57,21 +157,44 @@ const morseDecode = {
     "--..--":","
 };
 
-document.querySelector("input[name=swapKeys]").addEventListener("change", ({ target }) => {
-    swapKeys = target.checked;
-    document.querySelector("#key-dit").textContent = swapKeys ? "X/." : "Z/,";
-    document.querySelector("#key-dah").textContent = swapKeys ? "Z/," : "X/.";
-});
+const modulator = new Modulator();
+const keyer = new Keyer();
 
-let modeB = false;
+keyer.onTxStart = () => {
+    modulator.output(true);
+    document.querySelector("#i-tx").style.backgroundColor = "rgb(52, 199, 89)";
+}
+
+keyer.onTxStop = () => {
+    modulator.output(false);
+    document.querySelector("#i-tx").style.backgroundColor = "rgb(142, 142, 142)";
+}
+
+keyer.onKey = (key) => {
+    document.querySelector("pre.code").textContent += key;
+};
+
+keyer.onWord = (code) => {
+    const text = (code === "\n") ? code : morseCode[code] || ""
+    document.querySelector("pre.text").textContent += text;
+};
+
 document.querySelector("input[name=modeB]").addEventListener("change", ({ target }) => {
-    modeB = target.checked;
+    keyer.modeB = target.checked;
 });
 
-let wpm = 15;
 document.querySelector("input[name=wpm]").addEventListener("change", ({ target }) => {
     const val = parseInt(target.value);
-    if(Number.isFinite(val)) wpm = val;
+    if(Number.isFinite(val)) keyer.wpm = val;
+});
+
+document.querySelector("input[name=modEn]").addEventListener("change", ({ target }) => {
+    modulator.enable = target.checked;
+});
+
+document.querySelector("input[name=freq]").addEventListener("change", ({ target }) => {
+    const val = parseInt(target.value);
+    if(Number.isFinite(val)) modulator.setFreq(val);
 });
 
 document.querySelector("#clear").addEventListener("click", () => {
@@ -79,124 +202,38 @@ document.querySelector("#clear").addEventListener("click", () => {
     document.querySelector("pre.code").textContent = "";
 });
 
-class Modulator {
-    actx = new AudioContext();
-    osc = this.actx.createOscillator();
-    gain = this.actx.createGain();
-    band = this.actx.createBiquadFilter();
-    
-    constructor() {
-        this.osc.frequency.value = 550;
-        this.osc.start();
-        this.osc.connect(this.gain);
-        this.gain.gain.value = 0.001;
-        this.gain.connect(this.band);
-        this.band.type = "bandpass";
-        this.band.frequency.value = 550;
-        this.band.Q.value = 3;
-        this.band.connect(this.actx.destination);
-    }
-
-    start() {
-        this.gain.gain.value = 1;
-        document.querySelector("#i-tx").style.backgroundColor = "rgb(52, 199, 89)";
-    }
-
-    stop() {
-        this.gain.gain.value = 0;
-        document.querySelector("#i-tx").style.backgroundColor = "rgb(142, 142, 142)";
+let swapKeys = false;
+function mapKey(code) {
+    switch(code) {
+        case "KeyZ":
+        case "Comma":
+            return swapKeys ? "-" : ".";
+        case "KeyX":
+        case "Period":
+            return swapKeys ? "." : "-";
     }
 }
+document.querySelector("input[name=swapKeys]").addEventListener("change", ({ target }) => {
+    swapKeys = target.checked;
+    document.querySelector("#key-dit").textContent = swapKeys ? "X/." : "Z/,";
+    document.querySelector("#key-dah").textContent = swapKeys ? "Z/," : "X/.";
+});
 
-class Keyer {
-    dit = false;
-    dah = false;
-    prevKey = null;
-    nextKey = null;
-    idleUnits = 7;
-    code = "";
-    modulator = new Modulator();
-
-    constructor() {
-        document.body.addEventListener("keydown", ({ code, repeat }) => {
-            if(repeat) return;
-            const k = mapKey(code);
-            if(k === ".") this.dit = true;
-            if(k === "-") this.dah = true;
-            if(k && !this.nextKey) this.nextKey = k;
-            this.syncIndicators();
-        });
-        document.body.addEventListener("keyup", ({ code }) => {
-            if(mapKey(code) === ".") this.dit = false;
-            if(mapKey(code) === "-") this.dah = false;
-            this.syncIndicators();
-        });
-        setTimeout(() => this.run(), 0);
-    }
-
-    syncIndicators() {
-        document.querySelector("#i-dit").style.backgroundColor = this.dit ? "rgb(255, 149, 0)" : "rgb(142, 142, 142)";
-        document.querySelector("#i-dah").style.backgroundColor = this.dah ? "rgb(255, 149, 0)" : "rgb(142, 142, 142)";
-    }
-
-    timeout(unit) {
-        return new Promise(r => setTimeout(r, unit * wpm2unit(wpm)));
-    }
-
-    async append(key) {
-        document.querySelector("pre.code").textContent += key;
-        if(key === "\n") {
-            document.querySelector("pre.text").textContent += "\n";
-        }
-        if(key === " ") {
-            document.querySelector("pre.text").textContent += morseDecode[this.code] || "";
-            this.code = "";
-        }
-        if(key === ".") {
-            this.code += ".";
-            this.modulator.start();
-            await this.timeout(1);
-            this.modulator.stop();
-            await this.timeout(1);
-        }
-        if(key === "-") {
-            this.code += "-";
-            this.modulator.start();
-            await this.timeout(3);
-            this.modulator.stop();
-            await this.timeout(1);
-        }
-    }
-
-    async run() {
-        while(true) {
-            let currKey = null;
-            if(this.nextKey) {
-                [this.nextKey, currKey] = [null, this.nextKey];
-            } else if(this.dit && this.dah) {
-                if(this.prevKey === ".") currKey = "-";
-                if(this.prevKey === "-") currKey = ".";
-            } else if(this.dit) {
-                currKey = ".";
-            } else if(this.dah) {
-                currKey = "-";
-            } else {
-                const iu = this.idleUnits++;
-                if(iu < 1 && this.idleUnits >= 1 && modeB) {
-                    if(this.prevKey === ".") await this.append("-");
-                    if(this.prevKey === "-") await this.append(".");
-                    continue;
-                }
-                if(iu < 3 && this.idleUnits >= 3) this.append(" ");
-                if(iu < 7 && this.idleUnits >= 7) this.append("\n");
-                await this.timeout(1);
-                continue;
-            }
-            await this.append(currKey);
-            this.prevKey = currKey;
-            this.idleUnits = 0;
-        }
-    }
+function syncIndicators({ dit, dah }) {
+    document.querySelector("#i-dit").style.backgroundColor = dit ? "rgb(255, 149, 0)" : "rgb(142, 142, 142)";
+    document.querySelector("#i-dah").style.backgroundColor = dah ? "rgb(255, 149, 0)" : "rgb(142, 142, 142)";
 }
 
-window.keyer = new Keyer();
+document.body.addEventListener("keydown", ({ code, repeat }) => {
+    if(repeat) return;
+    const k = mapKey(code);
+    if(k === ".") keyer.dit = true;
+    if(k === "-") keyer.dah = true;
+    if(k) keyer.queue(k);
+    syncIndicators(keyer);
+});
+document.body.addEventListener("keyup", ({ code }) => {
+    if(mapKey(code) === ".") keyer.dit = false;
+    if(mapKey(code) === "-") keyer.dah = false;
+    syncIndicators(keyer);
+});
